276677f67aace3ec0fd54920989a1c49dc6faa67
N3iKos <82366850+N3iKos@users.noreply.github.com>
2026-06-03 14:45:44 +0700

    Add files via upload

--- script/SM/util.py
+++ script/SM/util.py
@@ -390,7 +390,7 @@ def change_key(line):
         display(HTML(f'<style>{Path(css).read_text()}</style>'))
 
     def key_inject(civitai_key, hf_token):
-        SyS(f'curl -sLo {nenen} https://github.com/n3iKos/segsmaker-fast/raw/main/script/nenen88.py')
+        SyS(f'curl -sLo {nenen} https://github.com/gutris1/segsmaker/raw/main/script/nenen88.py')
 
         p = Path(nenen)
         v = p.read_text()
